52095351114337ab488a46a774293209c8ef4e5a
Igor Trifunovic <igor.trifunovic@niji.fr>
2015-01-15 17:49:43 +0100

    Create manually a class and refresh pages to see that an instance is created as for local or connected server mode

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,18 @@
 $(function() {
   Parse.$ = jQuery;
+  console.log(Parse);
   
   Parse.initialize("rBAyYVKtLXu0SqO4751YQb1P7aa1t7oGzym15dzb", "V02n1O65AhwAaVWtD8pFkj1OF5C26YGmfk3lfx16");
 
   var TestObject = Parse.Object.extend("TestObject");
   var testObject = new TestObject();
   testObject.save({foo: "bar"}).then(function(object) {
-    alert("yay! it worked");
+    console.log("yay! it worked");
+  });
+  
+  var TestObject2 = Parse.Object.extend("TestObject2");
+  var testObject2 = new TestObject2();
+  testObject2.save({toto: "bar"}).then(function(object) {
+    console.log("An other instance of a new Class created!");
   });
 });
