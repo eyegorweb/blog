9974921b09340135eea5a5ed236a4b919558da02
Igor Trifunovic <igor.trifunovic@niji.fr>
2015-01-23 16:56:26 +0100

    add dependency: underscorejs

--- js/blog.js
+++ js/blog.js
@@ -1,29 +1,19 @@
 $(function() {
   Parse.$ = jQuery;
-  console.log(Parse);
+ //console.log(Parse);
   
   Parse.initialize("rBAyYVKtLXu0SqO4751YQb1P7aa1t7oGzym15dzb", "V02n1O65AhwAaVWtD8pFkj1OF5C26YGmfk3lfx16");
   
   var TestObject = Parse.Object.extend("TestObject");
   var testObject = new TestObject();
   testObject.save({foo: "bar"}).then(function(object) {
-    console.log("yay! it worked");
+    //console.log("yay! it worked");
   });
   
   var TestObject2 = Parse.Object.extend("TestObject2");
   var testObject2 = new TestObject2();
   testObject2.save({toto: "bar"}).then(function(object) {
-    console.log("An other instance of a new Class created!");
+    //console.log("An other instance of a new Class created!");
   });
-  
-  // Backbone instances
-  var User = Backbone.Model.extend
-  ( // Extend has 2 arguments
-    { // First one is an object => instance properties of the model
-      // Instance properties
-    },{ // Second is optionnal and is a class property hash
-      // Class properties
-    }
-  );
-  console.log(User.instanceProperty);
+
 });
